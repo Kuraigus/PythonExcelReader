2d8a81c5940705b397cf5052715f4207eae611ea
kuraigus <kuraigus@hotmail.com>
2020-10-27 01:44:58 -0300

    fixing percentages

--- index.py
+++ index.py
@@ -6,7 +6,8 @@ cidade = {}
 
 file = "Mapa de Oportunidades - Brasíla - 11.09.2020.xlsb"
 print("lendo planilha...")
-sheet = pd.read_excel(file, engine='pyxlsb',sheet_name = "BASE INFORMAÇÕES", skiprows=16, usecols=["BAIRRO / CIDADE", 'HPs CADASTRADOS', 'INSTALADOS', ' HP LIVRE'])
+sheet = pd.read_excel(file, engine='pyxlsb', sheet_name="BASE INFORMAÇÕES", skiprows=16, usecols=[
+                      "BAIRRO / CIDADE", 'HPs CADASTRADOS', 'INSTALADOS', ' HP LIVRE'])
 
 print("estruturando dados...")
 for index, row in sheet.iterrows():
@@ -18,7 +19,6 @@ for index, row in sheet.iterrows():
         cidade[row["BAIRRO / CIDADE"]]["INSTALADOS"] = 0
     if "HP LIVRE" not in cidade[row["BAIRRO / CIDADE"]]:
         cidade[row["BAIRRO / CIDADE"]]["HP LIVRE"] = 0
-        
 
     cidade[row["BAIRRO / CIDADE"]]["HPs CADASTRADOS"] += row["HPs CADASTRADOS"]
     if math.isnan(row["INSTALADOS"]) == False:
@@ -28,23 +28,25 @@ for index, row in sheet.iterrows():
 
 for key, value in cidade.items():
     percentage = value["INSTALADOS"] * 100 / value["HPs CADASTRADOS"]
-    cidade[key]["penetracao"] = str(percentage) + "%"
+    cidade[key]["penetracao"] = "{:.2f}%".format(percentage)
 
-cidade_sorted = dict(sorted(cidade.items(), key=lambda k: k[1]['HPs CADASTRADOS'], reverse=True))
+cidade_sorted = dict(
+    sorted(cidade.items(), key=lambda k: k[1]['HPs CADASTRADOS'], reverse=True))
 
 print("criando arquivo excel...")
 # Create the workbook and sheet for Excel
 workbook = openpyxl.Workbook()
 new_sheet = workbook.active
 
-new_sheet.cell(row = 1, column = 1, value="BAIRRO / CIDADE")
-new_sheet.cell(row = 1, column = 2, value="HPs CADASTRADOS")
-new_sheet.cell(row = 1, column = 3, value="INSTALADOS")
-new_sheet.cell(row = 1, column = 4, value="HP LIVRE")
+new_sheet.cell(row=1, column=1, value="BAIRRO / CIDADE")
+new_sheet.cell(row=1, column=2, value="HPs CADASTRADOS")
+new_sheet.cell(row=1, column=3, value="INSTALADOS")
+new_sheet.cell(row=1, column=4, value="HP LIVRE")
+new_sheet.cell(row=1, column=5, value="PENETRAÇÃO")
 
 # openpyxl does things based on 1 instead of 0
 row = 2
-for key,values in cidade_sorted.items():
+for key, values in cidade_sorted.items():
     # Put the key in the first column for each key in the dictionary
     new_sheet.cell(row=row, column=1, value=key)
     column = 2
